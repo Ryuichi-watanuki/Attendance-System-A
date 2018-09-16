0607ba6fb19e159dce2a8c596a8f30f5af73078f
EC2 Default User <ec2-user@ip-172-31-16-107.us-east-2.compute.internal>
2018-09-16 07:04:29 +0000

    CSS軽くいじったのでテスト

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20180821134018) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated", default: false
+    t.boolean "activated", default: true
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
