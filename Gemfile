62659ea59056dfa6626fc0bcd551b5016a9c6c2e
EC2 Default User <ec2-user@ip-172-31-16-107.us-east-2.compute.internal>
2018-09-16 06:05:48 +0000

    ブランチテスト

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '4.2.2'
 gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
+gem 'pg', '0.20.0'
+
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
@@ -36,7 +38,7 @@ group :test do
   gem 'guard-minitest',           '2.4.4'
 end
 
-group :production do
-  gem 'pg', '0.20.0'
-  gem 'fog', '1.42'
-end
+# group :production do
+#   gem 'pg', '0.20.0'
+#   gem 'fog', '1.42'
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -39,17 +39,17 @@ User.create!(name:  "テスト一般ユーザ用",
                activated_at: Time.zone.now)
 end
 
-# マイクロポスト
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+# # マイクロポスト
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.microposts.create!(content: content) }
+# end
 
-# リレーションシップ
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+# # リレーションシップ
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
